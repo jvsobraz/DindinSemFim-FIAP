d701f09bb3a32241d6ea12eeaf7982f145ac85dc
jvsobraz <jvsobraz@hotmail.com>
2023-04-28 09:25:20 -0300

    Update Despesa.java

--- src/main/java/br/com/fiap/dindin/models/Despesa.java
+++ src/main/java/br/com/fiap/dindin/models/Despesa.java
@@ -3,6 +3,11 @@ package br.com.fiap.dindin.models;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
+
+import br.com.fiap.dindin.controllers.ContaController;
+import br.com.fiap.dindin.controllers.DespesaController;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,12 +22,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Despesa {
+public class Despesa{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
@@ -38,5 +46,15 @@ public class Despesa {
 
     @ManyToOne
     private Conta conta;
+
+    public EntityModel<Despesa> toEntityModel(){
+        return EntityModel.of(
+            this,
+            linkTo(methodOn(DespesaController.class).show(id)).withSelfRel(),
+            linkTo(methodOn(DespesaController.class).destroy(id)).withRel("delete"),
+            linkTo(methodOn(DespesaController.class).index(null, Pageable.unpaged())).withRel("all"),
+            linkTo(methodOn(ContaController.class).show(this.getConta().getId())).withRel("conta")
+        );
+    }
 
 }
