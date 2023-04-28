32b50e6840e726666dbba983563ec18b69bd444c
jvsobraz <jvsobraz@hotmail.com>
2023-04-28 09:24:03 -0300

    Update UsuarioController.java

--- src/main/java/br/com/fiap/dindin/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/dindin/controllers/UsuarioController.java
@@ -1,38 +1,42 @@
 package br.com.fiap.dindin.controllers;
 
-import org.aspectj.apache.bcel.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.dindin.models.Credencial;
 import br.com.fiap.dindin.models.Usuario;
 import br.com.fiap.dindin.repository.UsuarioRepository;
 import jakarta.validation.Valid;
 
 @RestController
-public class UsuarioController {
-    
+public class UsuarioControlle {
+
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    AuthenticationManager manager;
+
+    @Autowired
+    PasswordEncoder encoder;
+
     @PostMapping("/api/registrar")
     public ResponseEntity<Usuario> registrar(@RequestBody @Valid Usuario usuario){
+        usuario.setSenha(encoder.encode(usuario.getSenha()));
         repository.save(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 
     @PostMapping("/api/login")
     public ResponseEntity<Object> login(@RequestBody @Valid Credencial credencial){
-       var usuario = repository.findByEmailAndSenha(credencial.email(), credencial.senha());
-
-       if (usuario.isEmpty()) 
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-
+        manager.authenticate(credencial.toAuthentication());
         return ResponseEntity.ok().build();
-        
     }
-
+    
 }
