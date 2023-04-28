1986108dae34df3bc47f99f806040f127a995607
jvsobraz <jvsobraz@hotmail.com>
2023-04-28 09:29:54 -0300

    Update Credencial.java

--- src/main/java/br/com/fiap/dindin/models/Credencial.java
+++ src/main/java/br/com/fiap/dindin/models/Credencial.java
@@ -5,7 +5,7 @@ import org.springframework.security.core.Authentication;
 
 public record Credencial(String email, String senha) {
 
-    public AuthenticationService toAuthentication() {
+    public Authentication toAuthentication() {
         return new UsernamePasswordAuthenticationToken(email, senha);
     }
     
