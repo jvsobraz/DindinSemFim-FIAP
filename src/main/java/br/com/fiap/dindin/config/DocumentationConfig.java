0dbc3a84abe9a414c0aa42e01f43448dc5d375f8
jvsobraz <jvsobraz@hotmail.com>
2023-05-18 10:49:26 -0300

    Update DocumentationConfig.java

---
+++ src/main/java/br/com/fiap/dindin/config/DocumentationConfig.java
@@ -0,0 +1,30 @@
+package br.com.fiap.dindin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class DocumentationConfig {
+   
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                    .title("DinDin Sem Fim API")
+                    .version("V1")
+                    .description("Uma API para o app de controle de gastos pessoais.")
+                    .contact(new Contact().name("Joao Carlos").email("joao@fiap.com.br"))
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
+}
