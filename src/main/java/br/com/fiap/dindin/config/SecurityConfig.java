9befdee61f1bcfe8b05b8158e340d9e2677e6353
jvsobraz <jvsobraz@hotmail.com>
2023-09-05 09:25:24 -0300

    Profile Open

--- src/main/java/br/com/fiap/dindin/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/dindin/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.fiap.dindin.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -19,21 +20,32 @@ public class SecurityConfig {
     @Autowired
     AuthorizationFilter authorizationFilter;
 
+    @Autowired
+    Environment env;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http
+        http
                 .authorizeHttpRequests()
                     .requestMatchers(HttpMethod.POST, "/api/registrar").permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                     .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                    .anyRequest().authenticated()
+                //    .anyRequest().permitAll()
                 .and()
                 .csrf().disable()
                 .formLogin().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("open")){
+            http.authorizeHttpRequests().anyRequest().permitAll();
+        }else{
+            http.authorizeHttpRequests().anyRequest().authenticated();
+        }
+
+        return http.build();
+                
     }
 
     @Bean
