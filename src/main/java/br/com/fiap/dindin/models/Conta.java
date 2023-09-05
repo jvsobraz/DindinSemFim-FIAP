efff684f9688be229860da4d58bc6f26e063c640
jvsobraz <jvsobraz@hotmail.com>
2023-09-05 09:45:59 -0300

    Validate

--- src/main/java/br/com/fiap/dindin/models/Conta.java
+++ src/main/java/br/com/fiap/dindin/models/Conta.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,10 +25,15 @@ public class Conta {
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
     
     @JsonProperty(access = Access.WRITE_ONLY)
+    @Min(value = 0, message = "O saldo não pode ser negativo")
     private BigDecimal saldoInicial;
+
+
     private String icone;
     @JsonIgnore
     private boolean ativa = true;
