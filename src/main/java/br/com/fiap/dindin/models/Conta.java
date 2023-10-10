da56f659d426fa342ab5811233f1d5e5511268ae
jvsobraz <jvsobraz@hotmail.com>
2023-10-09 23:10:49 -0300

    Update

--- src/main/java/br/com/fiap/dindin/models/Conta.java
+++ src/main/java/br/com/fiap/dindin/models/Conta.java
@@ -3,8 +3,8 @@ package br.com.fiap.dindin.models;
 import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
